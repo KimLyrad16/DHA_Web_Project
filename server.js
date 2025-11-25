d1a91f4adb6d909cd9ea4064fb5e374687d23a97
Daryl Arabiana <Arabiana.Daryl@gmail.com>
2025-11-25 11:52:39 +0800

    Fix: Updated MySQL env config for Railway

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const localConfig = {
   host: "127.0.0.1",
   port: 3306,
   user: "root",
-  password: "0516name91daryL",
+  password: "QwDKDOlwdwiNYAvgmDyYYvOavTqDAwMK",
   database: "dha_fabrication_db"
 };
 
